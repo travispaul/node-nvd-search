aca332a4bdf46068911094976d8a2a2e27286628
Travis Paul <tr@vispaul.me>
2019-07-22 16:38:56 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,215 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib');
+const crypto = require('crypto');
+
+const request = require('request');
+const {forEachParallel, waterfall} = require('vasync');
+
+module.exports = class NVD {
+
+  constructor(options = {}) {
+    // Defaults and user supplied options combined
+    this.defaults = {
+      // Which feeds to download and keep up-to-date
+      feeds: [
+        'modified',
+        'recent',
+        '2019',
+        '2018',
+        '2017',
+        '2016',
+        '2015',
+        '2014',
+        '2013',
+        '2012',
+        '2011',
+        '2010',
+        '2009',
+        '2008',
+        '2007',
+        '2006',
+        '2005',
+        '2004',
+        '2003',
+        '2002',
+      ],
+
+      // XXX not yet implemented XXX
+      // Attempt to fetch all feeds between the last one listed in the `feeds` config
+      // and the current year. E.g. if the last year list in `feeds` is 2019, but the
+      // current year is 2022, also attempt to fetch and sync feeds for 2020, 2021
+      // and 2022 if they exist.
+      //includeCurrentYearlyFeeds: false,
+
+      // fetch `${rootPath}-${feed}.json.gz`, if you have a private cache
+      // you can override this to fetch from your private cache.
+      rootPath: 'https://nvd.nist.gov/feeds/json/cve/1.0/nvdcve-1.0',
+      
+      // Default location where the json files are stored
+      cacheDir: NVD.chooseDefaultCacheDir(),
+
+      // Create a local SQLite database for faster searching
+      searchCache: true
+    };
+    this.config = Object.assign({}, this.defaults, options);
+  }
+
+  static parseMetaFile(metaFile) {
+    const lines = metaFile.split('\r\n');
+    const result = {};
+    for (const line of lines) {
+      if (!line) continue;
+      const key = line.replace(/:.*$/, '');
+      const value = line.replace(/^.*?:/, '');
+      result[key] = value;
+    }
+    return result;
+  }
+
+  // If user didn't supply a cacheDir, try to pick something sensible based on:
+  // https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+  //
+  // $XDG_CACHE_HOME defines the base directory relative to which user specific
+  // non-essential data files should be stored.
+  //
+  // If $XDG_CACHE_HOME is either not set or empty, a default equal to
+  // $HOME/.cache should be used.
+  static chooseDefaultCacheDir() {
+    if (process.env.XDG_CACHE_HOME) {
+      return path.join(process.env.XDG_CACHE_HOME, 'nvd')
+    }
+    return path.join(os.homedir(), '.cache', 'nvd');
+  }
+
+  // Download metafile and convert to an object
+  static fetchMetaFile(ctx, done) {
+    const metaPath = `${ctx.config.rootPath}-${ctx.feed}.meta`;
+    request(metaPath, (error, response, body) => {
+      if (error) return done(error);
+      ctx.metadata = NVD.parseMetaFile(body);
+      done(null, ctx);
+    });
+  }
+
+  // Determine if local file matches the remoet metadata, if it doesn't set
+  // ctx.fetchRemote to true so the next function in the pipeline downloads
+  // the latest file.
+  static checkLocalFeedFile(ctx, done) {
+    const localFeedFile = `${ctx.config.cacheDir}/nvdcve-1.0-${ctx.feed}.json`;
+    const reader = fs.createReadStream(localFeedFile);
+    const hash = crypto.createHash('sha256');
+    hash.setEncoding('hex');
+
+    reader.on('end', () => {
+      hash.end();
+      const hashValue = hash.read().toUpperCase();
+      if (hashValue === ctx.metadata.sha256) {
+        done(null, ctx);
+      } else {
+        const error = `sha256 hash (${hashValue}) of ${localFeedFile} doesn't match metadata's hash: ${ctx.metadata.sha256}`;
+        done(new Error(error), ctx);
+      }
+    });
+
+    reader.on('error', (error) => {
+      reader.close();
+      if (error && error.code !== 'ENOENT') {
+        return done(error);
+      }
+      ctx.fetchRemote = true;
+      done(null, ctx);
+    });
+
+    reader.pipe(hash);
+  }
+
+  // Download and cache the specific remote feed file, unzip it and write it to
+  // disk. Only do this if ctx.fetchRemote is true (see checkLocalFeedFile)
+  static fetchRemoteFeedFile(ctx, done) {
+    if (!ctx.fetchRemote) {
+      return done(null, ctx);
+    }
+    const gzip = zlib.createGunzip();
+    const writer = fs.createWriteStream(`${ctx.config.cacheDir}/nvdcve-1.0-${ctx.feed}.json`);
+    const httpStream = request(`${ctx.config.rootPath}-${ctx.feed}.json.gz`);
+
+    httpStream.on('error', done);
+
+    gzip.on('close', () => {
+      done(null, ctx);
+    });
+
+    httpStream.pipe(gzip).pipe(writer);
+  }
+
+  // download feed metafile and download if remote differs from the loca file
+  static fetchFeedWaterfall (ctx, done) {
+    waterfall([
+        (next) => {
+          next(null, ctx);
+        },
+        NVD.fetchMetaFile,
+        NVD.checkLocalFeedFile,
+        NVD.fetchRemoteFeedFile
+    ], (error, ctx) => {
+        if (error) {
+          return done(error);
+        }
+        done(null, ctx);
+    });
+  }
+
+  // fetch all the remote feeds (if needed)
+  static fetchFeedParallel (contexts, done) {
+    forEachParallel({
+        func: NVD.fetchFeedWaterfall,
+        inputs: contexts
+    }, (error, results) => {
+        if (error) {
+            return done(error);
+        }
+        const simpleResults = results.operations.map((operation) => {
+          delete operation.result.config;
+          return operation.result;
+        }).filter(val => val);
+
+        done(null, simpleResults);
+    });
+  }
+
+  // Create the configured cache directory
+  static createCacheDir (ctx, next) {
+    fs.mkdir(ctx[0].config.cacheDir, {recursive: true}, (error) => {
+      if (error) {
+        return next(error, ctx);
+      }
+      next(null, ctx);
+    });
+  }
+
+  // sync remote files locally
+  sync(done) {
+
+    // Provide a copy of the config to each feed
+    const contexts = this.config.feeds.map((feed) => {
+      return {
+        feed,
+        config: this.config
+      };
+    });
+    waterfall([
+        (next) => {
+          next(null, contexts);
+        },
+        NVD.createCacheDir,
+        NVD.fetchFeedParallel,
+      ], (error, results) => {
+        if (error) {
+          return done(error, results);
+        }
+        done(null, results);
+    });
+  }
+}
---
+++ test/index.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+const url = require('url');
+const path = require('path');
+
+const {expect, assert} = require('chai');
+const nock = require('nock');
+
+const NVD = require('../index');
+
+describe('NVD Class', () => {
+  before(done => {
+
+    try {
+      fs.unlinkSync(path.join(__dirname, 'data', 'cache', 'nvdcve-1.0-modified.json'));
+    } catch (error) {
+      if (error.code !== 'ENOENT') {
+        throw error;
+      }
+    }
+    done();
+  });
+
+  describe('nvd.parseMetaFile()', () => {
+    it('Correctly parses a feed meta file to an object', done => {
+      const metaFileObj = require('./data/nvdcve-1.0-recent.meta.json');
+      const testFilePath = path.join(__dirname, 'data', 'nvdcve-1.0-recent.meta');
+      const metaFileContents = fs.readFileSync(testFilePath).toString('utf8');
+      const parsedMetaFile = NVD.parseMetaFile(metaFileContents);
+      expect(parsedMetaFile).to.deep.equal(metaFileObj);
+      done();
+    });
+  });
+
+  describe('nvd.sync()', () => {
+    it('Fetches all desired files', done => {
+      const nvd = new NVD({
+        feeds: ['recent', 'modified'],
+        cacheDir: path.join(__dirname, 'data', 'cache')
+      });
+
+      const syncURL = url.parse(nvd.config.rootPath);
+      const syncScope = nock(`${syncURL.protocol}//${syncURL.hostname}`)
+        .get(`${syncURL.pathname}-recent.meta`)
+        .reply(200, fs.readFileSync(path.join(__dirname, 'data',
+          'nvdcve-1.0-recent.meta')).toString('utf8'));
+
+      // syncScope.get(`${syncURL.pathname}-recent.json.gz`)
+      //   .reply(200, (uri, requestBody) => {
+      //     return fs.createReadStream(path.join(__dirname, 'data',
+      //       'nvdcve-1.0-recent.json.gz'));
+      //   });
+
+      syncScope.get(`${syncURL.pathname}-modified.meta`)
+        .reply(200, fs.readFileSync(path.join(__dirname, 'data',
+          'nvdcve-1.0-modified.meta')).toString('utf8'));
+
+      syncScope.get(`${syncURL.pathname}-modified.json.gz`)
+        .reply(200, (uri, requestBody) => {
+          return fs.createReadStream(path.join(__dirname, 'data',
+            'nvdcve-1.0-modified.json.gz'));
+        });
+
+      nvd.sync((error, results) => {
+        if (error) {
+          return console.error(error);
+        }
+        expect(results).to.deep.equal([{
+          feed: 'recent',
+          metadata: {
+            lastModifiedDate: '2019-07-19T20:00:55-04:00',
+            size: '4336300',
+            zipSize: '243985',
+            gzSize: '243845',
+            sha256: 'DA0B3030EF781806228ED40A7F295182AC835E96F4F5B27C258228531FBBED0C'
+          }
+        },
+        {
+          feed: 'modified',
+          metadata: {
+            lastModifiedDate: '2019-07-22T14:02:09-04:00',
+            size: '6575683',
+            zipSize: '354809',
+            gzSize: '354665',
+            sha256: '0BF42E5E2CF81A7F3F9A6F1581BF54D5E970185D96D3D9011ED63AA695FB7B2D'
+          },
+          fetchRemote: true
+        }]);
+        syncScope.done();
+        done();
+      });
+    });
+  });
+});
